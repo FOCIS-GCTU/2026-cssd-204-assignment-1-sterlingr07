ff3981bb95e6e6f61807cd88620f9ec4291d5f55
AbdulRahim Iddrisu <abdulrahim07d@gmail.com>
2026-06-01 07:21:22 -0800

    Add files via upload
    Assignment 1

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: AbdulRahim Iddrisu
+# STUDENT ID:  2425402236
+# Email: 2425402236@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, AbdulRahim Iddrisu, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print my initials in small and large stylized letters
 
+    print()
+    print("...AIS")
 
+    print("...AAAAAAAAAAAA........IIIIIIIIIIII........SSSSSSSSSSSS.....")
+    print("...AA........AA............IIII............SSSS.............")
+    print("...AA........AA............IIII............SSSS.............")
+    print("...AA........AA............IIII............SSSS.............")
+    print("...AAAAAAAAAAAA............IIII............SSSSSSSSSSSS.....")
+    print("...AA........AA............IIII....................SSSS.....")
+    print("...AA........AA............IIII....................SSSS.....")
+    print("...AA........AA............IIII....................SSSS.....")
+    print("...AA........AA............IIII....................SSSS.....")
+    print("...AA........AA........IIIIIIIIIIII........SSSSSSSSSSSS.....")
+
+    print()
 
 main()
